a34ee6b6492e7f452fa8abd83473171369dff57e
Sebastien Le Maguer <lemagues@tcd.ie>
2020-04-07 21:13:24 +0100

    [tools] add: mu

---
+++ src/20-tools/mu/install.sh
@@ -0,0 +1,47 @@
+#!/bin/zsh
+
+# Default values
+NB_PROC=1
+
+# Dealing with options
+while getopts ":j:hs" opt; do
+    case $opt in
+        j)
+            NB_PROC=$OPTARG
+            echo "parallel mode activated with $NB_PROC process" >&2
+            ;;
+        s)
+            echo "server mode installation activated" >&2
+            SERVER_MODE_ON=true
+            ;;
+        h)
+            echo "An help part should be done" >&2
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+shift $OPTIND-1
+
+if [ $# -lt 1 ]
+then
+    echo "$0 [-s] [-j <nb_proc>] <prefix>"
+    exit -1
+fi
+
+PREFIX=$1
+
+if [ "$SERVER_MODE_ON" != true ]
+then
+    (
+        git clone git@github.com:djcb/mu.git
+        cd mu
+        ./autogen.sh --prefix=$PREFIX
+        make -j $NB_PROC
+        make install
+        cd ..
+        rm -rfv mu
+
+    )
+fi
