34f9c7088d021d327d9490f6408d3d2e79545fba
Sebastien Le Maguer <lemagues@tcd.ie>
2020-04-08 20:42:05 +0100

    [tools] add: lsp servers (dockerfile, groovy and kotlin)

---
+++ src/20-tools/lsp/groovy/install.sh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+
+# Default values
+NB_PROC=1
+
+# Dealing with options
+while getopts ":j:hs" opt; do
+    case $opt in
+        j)
+            NB_PROC=$OPTARG
+            echo "parallel mode activated with $NB_PROC process" >&2
+            ;;
+        s)
+            echo "server mode installation activated" >&2
+            SERVER_MODE_ON=true
+            ;;
+        h)
+            echo "An help part should be done" >&2
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+shift $OPTIND-1
+
+if [ $# -lt 1 ]
+then
+    echo "$0 [-s] [-j <nb_proc>] <prefix>"
+    exit -1
+fi
+
+PREFIX=$1
+
+if [ "$SERVER_MODE_ON" != true ]
+then
+    (
+        git clone git@github.com:prominic/groovy-language-server.git
+        cd groovy-language-server
+        ./gradlew build
+        mkdir -p $PREFIX/lib/
+        cp -rfv build/libs/groovy-language-server.jar $PREFIX/lib
+        cd ../
+        rm -rfv groovy-language-server
+    )
+fi
---
+++ src/20-tools/lsp/kotlin/install.sh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+
+# Default values
+NB_PROC=1
+
+# Dealing with options
+while getopts ":j:hs" opt; do
+    case $opt in
+        j)
+            NB_PROC=$OPTARG
+            echo "parallel mode activated with $NB_PROC process" >&2
+            ;;
+        s)
+            echo "server mode installation activated" >&2
+            SERVER_MODE_ON=true
+            ;;
+        h)
+            echo "An help part should be done" >&2
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+shift $OPTIND-1
+
+if [ $# -lt 1 ]
+then
+    echo "$0 [-s] [-j <nb_proc>] <prefix>"
+    exit -1
+fi
+
+PREFIX=$1
+
+if [ "$SERVER_MODE_ON" != true ]
+then
+    (
+        git clone git@github.com:fwcd/kotlin-language-server.git
+        cd kotlin-language-server
+        ./gradlew :server:installDist
+        rsync -avP server/build/install/server/bin/ $PREFIX/bin
+        rsync -avP server/build/install/server/lib/ $PREFIX/lib
+        cd ../
+        rm -rfv kotlin-language-server
+    )
+fi
