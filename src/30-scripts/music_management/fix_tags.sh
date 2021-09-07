3f883b5cdcc4b87bce977f8a3ea2438333016f6e
Sebastien Le Maguer <lemagues@tcd.ie>
2021-09-07 09:59:58 +0100

    [scripts] update: fix_tags to support directly folder

--- src/30-scripts/music_management/fix_tags.sh
+++ src/30-scripts/music_management/fix_tags.sh
@@ -2,25 +2,34 @@
 
 if [ $# -ne 2 ]
 then
-    echo "Command should be: $0 <track_file> <root_dir>"
+    echo "Command should be: $0 <album_dir> <root_dir>"
     exit -1
 fi
 
 # Have better variable names
-track_file=$1
-root_dir=$2
+album_dir=$(realpath $1)
+root_dir=$(realpath $2)
 
-# Extract proper variables
-part_to_parse=$(echo $track_file | sed "s%$root_dir%%g")
-ARTIST=$(echo $part_to_parse | sed "s%/.*%%g")
-ALBUM=$(echo $part_to_parse | sed "s%[^/]*/[0-9]\+ - \([^/]*\)/.*%\1%g")
-DATE=$(echo $part_to_parse | sed "s%[^/]*/\([0-9]\+\) - .*%\1%g")
-TRACKNUMBER=$(echo $part_to_parse | sed "s%[^/]*/[^/]*/\([0-9]\+\) - .*%\1%g")
-TITLE=$(echo $part_to_parse | sed "s%[^/]*/[^/]*/[0-9]\+ - \(.*\).flac%\1%g")
+shopt -s nullglob
+list_tracks=("$album_dir"/*.flac)
+shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
 
-# Fix tags
-tag=ALBUM;       metaflac --remove-tag=$tag "--set-tag=$tag=$ALBUM" "$track_file"
-tag=ARTIST;      metaflac --remove-tag=$tag "--set-tag=$tag=$ARTIST" "$track_file"
-tag=TITLE;       metaflac --remove-tag=$tag "--set-tag=$tag=$TITLE" "$track_file"
-tag=TRACKNUMBER; metaflac --remove-tag=$tag "--set-tag=$tag=$TRACKNUMBER" "$track_file"
-tag=DATE;        metaflac --remove-tag=$tag "--set-tag=$tag=$DATE" "$track_file"
+IFS=$'\n'
+for track_file in ${list_tracks[@]}
+do
+    # Extract proper variables
+    part_to_parse=$(echo $track_file | sed "s%$root_dir/%%g")
+    ARTIST=$(echo $part_to_parse | sed "s%/.*%%g")
+    ALBUM=$(echo $part_to_parse | sed "s%[^/]*/[0-9]\+ - \([^/]*\)/.*%\1%g")
+    DATE=$(echo $part_to_parse | sed "s%[^/]*/\([0-9]\+\) - .*%\1%g")
+    TRACKNUMBER=$(echo $part_to_parse | sed "s%[^/]*/[^/]*/\([0-9]\+\) - .*%\1%g")
+    TITLE=$(echo $part_to_parse | sed "s%[^/]*/[^/]*/[0-9]\+ - \(.*\).flac%\1%g")
+
+    # Fix tags
+    tag=ALBUM;       metaflac --remove-tag=$tag "--set-tag=$tag=$ALBUM" "$track_file"
+    tag=ARTIST;      metaflac --remove-tag=$tag "--set-tag=$tag=$ARTIST" "$track_file"
+    tag=TITLE;       metaflac --remove-tag=$tag "--set-tag=$tag=$TITLE" "$track_file"
+    tag=TRACKNUMBER; metaflac --remove-tag=$tag "--set-tag=$tag=$TRACKNUMBER" "$track_file"
+    tag=DATE;        metaflac --remove-tag=$tag "--set-tag=$tag=$DATE" "$track_file"
+
+done
