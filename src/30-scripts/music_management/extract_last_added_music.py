152694c14faead57e805812043e0b0c73a757873
Sebastien Le Maguer <lemagues@tcd.ie>
2022-05-16 07:45:57 +0100

    [scripts] add: helper to extract last added music to a specific folder

---
+++ src/30-scripts/music_management/extract_last_added_music.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AUTHOR
+
+    Sébastien Le Maguer <lemagues@tcd.ie>
+
+DESCRIPTION
+
+LICENSE
+    This script is in the public domain, free from copyrights or restrictions.
+    Created: 13 May 2022
+"""
+
+# Arguments
+import argparse
+
+# Messaging/logging
+import logging
+from logging.config import dictConfig
+
+#
+import pathlib
+import shutil
+import datetime
+
+###############################################################################
+# global constants
+###############################################################################
+LEVEL = [logging.WARNING, logging.INFO, logging.DEBUG]
+
+###############################################################################
+# Functions
+###############################################################################
+def configure_logger(args) -> logging.Logger:
+    """Setup the global logging configurations and instanciate a specific logger for the current script
+
+    Parameters
+    ----------
+    args : dict
+        The arguments given to the script
+
+    Returns
+    --------
+    the logger: logger.Logger
+    """
+    # create logger and formatter
+    logger = logging.getLogger()
+
+    # Verbose level => logging level
+    log_level = args.verbosity
+    if args.verbosity >= len(LEVEL):
+        log_level = len(LEVEL) - 1
+        # logging.warning("verbosity level is too high, I'm gonna assume you're taking the highest (%d)" % log_level)
+
+    # Define the default logger configuration
+    logging_config = dict(
+        version=1,
+        disable_existing_logger=True,
+        formatters={
+            "f": {
+                "format": "[%(asctime)s] [%(levelname)s] — [%(name)s — %(funcName)s:%(lineno)d] %(message)s",
+                "datefmt": "%d/%b/%Y: %H:%M:%S ",
+            }
+        },
+        handlers={
+            "h": {
+                "class": "logging.StreamHandler",
+                "formatter": "f",
+                "level": LEVEL[log_level],
+            }
+        },
+        root={"handlers": ["h"], "level": LEVEL[log_level]},
+    )
+
+    # Add file handler if file logging required
+    if args.log_file is not None:
+        logging_config["handlers"]["f"] = {
+            "class": "logging.FileHandler",
+            "formatter": "f",
+            "level": LEVEL[log_level],
+            "filename": args.log_file,
+        }
+        logging_config["root"]["handlers"] = ["h", "f"]
+
+    # Setup logging configuration
+    dictConfig(logging_config)
+
+    # Retrieve and return the logger dedicated to the script
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+def define_argument_parser() -> argparse.ArgumentParser:
+    """Defines the argument parser
+
+    Returns
+    --------
+    The argument parser: argparse.ArgumentParser
+    """
+    parser = argparse.ArgumentParser(description="")
+
+    # Add options
+    parser.add_argument("-l", "--log_file", default=None, help="Logger file")
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        action="count",
+        default=0,
+        help="increase output verbosity",
+    )
+
+    # Add arguments
+    parser.add_argument("in_dir")
+    parser.add_argument("since_date", help="Date (format = YYYY-mm-dd) ")
+    parser.add_argument("out_dir")
+
+    # Return parser
+    return parser
+
+
+###############################################################################
+#  Envelopping
+###############################################################################
+if __name__ == "__main__":
+    # Initialization
+    arg_parser = define_argument_parser()
+    args = arg_parser.parse_args()
+    logger = configure_logger(args)
+
+    # TODO: your code comes here
+    since_date = datetime.datetime.strptime(args.since_date, "%Y-%m-%d")
+    album_dir_set = set()
+    in_dir = pathlib.Path(args.in_dir)
+    out_dir = pathlib.Path(args.out_dir)
+    for i in in_dir.glob('**/*.flac'):
+        m_time = datetime.datetime.fromtimestamp(i.stat().st_mtime)
+        if since_date < m_time:
+            rel_part = str(i.parent).replace(str(in_dir) + "/", "")
+            tgt_dir = out_dir/rel_part
+            if tgt_dir not in album_dir_set:
+                album_dir_set.add(tgt_dir)
+                logger.info(f"Copy {i.parent}")
+                shutil.copytree(i.parent, tgt_dir)
