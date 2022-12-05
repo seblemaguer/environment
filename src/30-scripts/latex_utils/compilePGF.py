89cfe23250195bb551e0fac627d9a9b71df9b169
Sebastien Le Maguer <lemagues@tcd.ie>
2022-12-05 11:51:41 +0000

    [scripts] update: compilePGF to support user defined header

--- src/30-scripts/latex_utils/compilePGF.py
+++ src/30-scripts/latex_utils/compilePGF.py
@@ -37,8 +37,7 @@ LEVEL = [logging.WARNING, logging.INFO, logging.DEBUG]
 # Main function
 ###############################################################################
 def main():
-    """Main entry function
-    """
+    """Main entry function"""
     global args
 
     # Get output directory
@@ -53,9 +52,11 @@ def main():
     if not args.with_svg:
         header = "\\documentclass[crop,tikz,multi=false]{standalone}[2012/04/13]"
 
+    if args.header_file is not None:
+        with open(args.header_file) as f_header:
+            cont = str(f_header.read())
+            header += f"\n\n%% Additional Header\n{cont}\n\n"
     tex_content = """%s
-    \\usepackage{pgfplots}
-    \\DeclareUnicodeCharacter{2212}{−}
 
     %% fonts and encoding
     \\usepackage{lmodern}
@@ -87,6 +88,9 @@ def main():
     \\usepackage{multicol}
     \\usepackage{hhline}
 
+    %%  = Tikz and Plot
+    \\usepackage{pgfplots}
+    \\DeclareUnicodeCharacter{2212}{−}
     \\usepgfplotslibrary{groupplots,dateplot}
     \\usetikzlibrary{patterns,shapes.arrows}
     \\pgfplotsset{compat=newest}
@@ -94,7 +98,11 @@ def main():
     \\begin{document}
     \\input{%s}
     \\end{document};
-    """ % (header, parent_directory, pgf_filename)
+    """ % (
+        header,
+        parent_directory,
+        pgf_filename,
+    )
 
     # Create temporary part
     temp_dir = tempfile.mkdtemp()
@@ -117,41 +125,72 @@ def main():
         tex_fh.write(tex_content)
 
     # Compile
-    call(["pdflatex", "-interaction=nonstopmode", "-shell-escape", "%s.tex" % basename], cwd=temp_dir)
+    call(
+        ["pdflatex", "-interaction=nonstopmode", "-shell-escape", "%s.tex" % basename],
+        cwd=temp_dir,
+    )
 
     # Copy
-    shutil.copyfile("%s/%s.pdf" % (temp_dir, basename), "%s/%s.pdf" % (output_directory, basename))
+    call(
+        ["pdfcrop", f"{basename}.pdf", "%s/%s.pdf" % (output_directory, basename)],
+        cwd=temp_dir
+    )
+    # shutil.copyfile(
+    #     "%s/%s.pdf" % (temp_dir, basename), "%s/%s.pdf" % (output_directory, basename)
+    # )
     if args.with_svg:
-        shutil.copyfile("%s/%s.svg" % (temp_dir, basename), "%s/%s.svg" % (output_directory, basename))
+        shutil.copyfile(
+            "%s/%s.svg" % (temp_dir, basename),
+            "%s/%s.svg" % (output_directory, basename),
+        )
 
     # Delete temps files
     shutil.rmtree(temp_dir)
 
+
 ###############################################################################
 #  Envelopping
 ###############################################################################
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser(description="")
 
         # Add options
-        parser.add_argument("-v", "--verbosity", action="count", default=0,
-                            help="increase output verbosity")
-        parser.add_argument("-p", "--with-svg", action="store_true",
-                            help="Produce the SVG as well")
-        parser.add_argument("-o", "--output_directory", default=None,
-                            help="output directory")
+        parser.add_argument(
+            "-H",
+            "--header_file",
+            default=None,
+            help="Header file to add commands or packages",
+        )
+        parser.add_argument(
+            "-v",
+            "--verbosity",
+            action="count",
+            default=0,
+            help="increase output verbosity",
+        )
+        parser.add_argument(
+            "-p", "--with-svg", action="store_true", help="Produce the SVG as well"
+        )
+        parser.add_argument(
+            "-o", "--output_directory", default=None, help="output directory"
+        )
 
         # Add arguments
-        parser.add_argument("pgf_filename", help="filename of the tikz/pgf file (extension should not be .tex!)")
+        parser.add_argument(
+            "pgf_filename",
+            help="filename of the tikz/pgf file (extension should not be .tex!)",
+        )
 
         # Parsing arguments
         args = parser.parse_args()
 
         # Verbose level => logging level
         log_level = args.verbosity
-        if (args.verbosity > len(LEVEL)):
-            logging.warning("verbosity level is too high, I'm gonna assume you're taking the highes ")
+        if args.verbosity > len(LEVEL):
+            logging.warning(
+                "verbosity level is too high, I'm gonna assume you're taking the highes "
+            )
             log_level = len(LEVEL) - 1
         logging.basicConfig(level=LEVEL[log_level])
 
@@ -164,8 +203,9 @@ if __name__ == '__main__':
 
         # Debug time
         logging.info("end time = " + time.asctime())
-        logging.info('TOTAL TIME IN MINUTES: %02.2f' %
-                     ((time.time() - start_time) / 60.0))
+        logging.info(
+            "TOTAL TIME IN MINUTES: %02.2f" % ((time.time() - start_time) / 60.0)
+        )
 
         # Exit program
         sys.exit(0)
@@ -174,7 +214,7 @@ if __name__ == '__main__':
     except SystemExit as e:  # sys.exit()
         pass
     except Exception as e:
-        logging.error('ERROR, UNEXPECTED EXCEPTION')
+        logging.error("ERROR, UNEXPECTED EXCEPTION")
         logging.error(str(e))
         traceback.print_exc(file=sys.stderr)
         sys.exit(-1)
