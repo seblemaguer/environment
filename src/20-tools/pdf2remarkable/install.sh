58521567472676500aedfeada8e2a34768bc9d71
Sebastien Le Maguer <lemagues@tcd.ie>
2023-01-06 19:20:23 +0000

    [tools] add: pdf2remarkable

---
+++ src/20-tools/pdf2remarkable/install.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+# Set environment path to the current directory
+NB_PROC=1
+
+# Dealing with options
+while getopts ":j:hs" opt; do
+    case $opt in
+        j)
+            NB_PROC=$OPTARG
+            echo "parallel mode activated with $NB_PROC process" >&2
+            ;;
+        s)
+            echo "server mode installation activated" >&2
+            SERVER_MODE_ON=true
+            ;;
+        h)
+            echo "An help part should be done" >&2
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+shift $OPTIND-1
+
+# Dealing with arguments
+if [ $# -lt 1 ]
+then
+    echo "$0 [-s] [-j <nb_proc>] <prefix>"
+    exit -1
+fi
+PREFIX=$1
+
+# Installing different part
+if [ "$SERVER_MODE_ON" != true ]
+then
+    git clone git@github.com:adaerr/reMarkableScripts.git
+    (
+        cd reMarkableScripts
+        cp -rfv pdf2remarkable.sh $PREFIX/bin/pdf2remarkable
+        rm -rfv reMarkableScripts
+    )
+fi
