b35a021c46ac9943c465e4d65f14c88e06e9b97a
Sebastien Le Maguer <lemagues@tcd.ie>
2024-03-15 13:35:04 +0200

    [scripts] update: cleaner project creation

--- src/30-scripts/work/create_project.py
+++ src/30-scripts/work/create_project.py
@@ -28,7 +28,7 @@ class ProjectDirectoryStructure:
             "experiments": [],
             "data": ["raw_data", "processed_data", "results", "metadata"],
             "tools": [],
-            "resources": ["external_resources"],
+            "resources": ["external_resources", "figures"],
         }
 
     def create_directory(self, directory_path: Path) -> None:
@@ -48,11 +48,11 @@ class ProjectDirectoryStructure:
             directory_path (Path): The path of the directory to create the README for.
         """
         with open(directory_path / "README.org", "w") as f:
-            f.write(
-                f"#+TITLE: {directory_path.name.capitalize()}\n\n"
-            )
+            f.write(f"#+TITLE: {directory_path.name.capitalize()}\n\n")
             f.write("* Description \n\n")
-            f.write("* References\n\nUse org-transclusion here to insert files from the org-roam part\n\n")
+            f.write(
+                "* References\n\nUse org-transclusion here to insert files from the org-roam part\n\n"
+            )
 
     def create_directory_structure(self) -> None:
         """Create the directory structure for the project."""
@@ -63,7 +63,7 @@ class ProjectDirectoryStructure:
 
         # Create subdirectories and README files
         for directory, subdirs in self.directories.items():
-            directory_path = root_dir/ directory
+            directory_path = root_dir / directory
             self.create_directory(directory_path)
 
             for subdir in subdirs:
@@ -105,13 +105,48 @@ class PersonalProjectDirectoryStructure(ProjectDirectoryStructure):
                 self.create_directory(subdir_path)
 
 
+class SupportProjectDirectoryStructure(ProjectDirectoryStructure):
+    """Class to create directory structure for a support project."""
+
+    def __init__(self, project_name: str):
+        """
+        Initialize SupportProjectDirectoryStructure.
+
+        Args:
+            project_name (str): The name of the project.
+        """
+        super().__init__(project_name)
+
+        self.directories: Dict[str, List[str]] = {
+            "code": ["src", "tests"],
+            "data": [],
+            "docs": ["documentation"],
+        }
+
+    def create_directory_structure(self) -> None:
+        """Create the directory structure for the support project."""
+        # Create main project directory
+        root_dir = Path(self.project_name)
+        self.create_directory(root_dir)
+        self.create_readme(root_dir)
+
+        # Create subdirectories and README files
+        for directory, subdirs in self.directories.items():
+            directory_path = root_dir / directory
+            self.create_directory(directory_path)
+
+            for subdir in subdirs:
+                subdir_path = directory_path / subdir
+                self.create_directory(subdir_path)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Create directory structure for research or personal project."
     )
     parser.add_argument(
-        "--project-type",
-        choices=["research", "personal"],
+        "-t", "--project-type",
+        choices=["research", "personal", "support"],
         default="research",
         help="Type of project: research or personal",
     )
@@ -122,6 +157,8 @@ if __name__ == "__main__":
         project = ProjectDirectoryStructure(args.project_name)
     elif args.project_type == "personal":
         project = PersonalProjectDirectoryStructure(args.project_name)
+    elif args.project_type == "support":  # Add condition for "support" project type
+        project = SupportProjectDirectoryStructure(args.project_name)
     else:
         print("Invalid project type specified.")
         exit(1)
