452f923f6560d48b54651d4e179e9947aaacb423
Sebastien Le Maguer <sebastien.lemaguer@helsinki.fi>
2026-02-12 16:25:52 +0200

    [tools] fix: options/docs of gradle-export wrapper

--- src/20-tools/gradle-org-export/assets/gradle_export.sh
+++ src/20-tools/gradle-org-export/assets/gradle_export.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 # Set environment path to the current directory
-PLUGIN_PATH=###PREFIX###/apps/gradle-org-export-plugin
+PLUGIN_PATH=/home/lemaguer/environment/local/apps/gradle-org-export-plugin
 NB_PROC=1
 
 # Dealing with options
-while getopts ":j:dihs" opt; do
+while getopts ":j:dinMhsV" opt; do
     case $opt in
         j)
             NB_PROC=$OPTARG
@@ -16,8 +16,17 @@ while getopts ":j:dihs" opt; do
         i)
             INFO_MODE=true
             ;;
+        M)
+            WITHOUT_MULTIMEDIA=true
+            ;;
+        n)
+            WITH_NOTES=true
+            ;;
         s)
             STACKTRACE_MODE=true
+            ;;
+        V)
+            WITHOUT_VIDEO=true
             ;;
         h)
             HELP_MODE=true
@@ -32,10 +41,16 @@ shift $OPTIND-1
 # Dealing with arguments
 if [[ $# -lt 1 ]]; then
     echo "$0 [[-j <nb_proc>]] [[-s]] [[-i]] [[-d]] <export_type>"
-    echo "  -j parallel mode (if nb_proc is 0, use all the processor, if )"
+
+    # Default modes
+    echo "  -j parallel mode (if nb_proc is 0, use all the processor, else use the given number of workers)"
     echo "  -d activate gradle debug mode"
     echo "  -i activate gradle info mode"
     echo "  -s activate gradle stack mode"
+
+    echo "  -n insert note slides"
+    echo "  -M *DISABLE* multimedia assets"
+    echo "  -V *DISABLE* videos assets"
 
     if [[ "$HELP_MODE" == true ]]; then
         exit 0
@@ -56,7 +71,10 @@ fi
 # Define verbosity and debugging options
 [[ "$DEBUG_MODE" == true ]] && DEBUG_OPT="--debug"
 [[ "$INFO_MODE" == true ]] && INFO_OPT="--info"
+[[ "$WITH_NOTES" == true ]] && WITH_NOTES_OPT="-Pwith_notes=TRUE"
+[[ "$WITHOUT_MULTIMEDIA" == true ]] && WITHOUT_MULTIMEDIA_OPT="-Pwithout_multimedia=TRUE"
+[[ "$WITHOUT_VIDEO" == true ]] && WITHOUT_VIDEO_OPT="-Pwithout_video=TRUE"
 [[ "$STACKTRACE_MODE" == true ]] && STACKTRACE_OPT="--stacktrace"
 
 # Ready to run the command now
-./gradlew --include-build="$PLUGIN_PATH" $DEBUG_OPT $PARALLEL_OPT $INFO_OPT $STACKTRACE_OPT $EXPORT_TASK
+./gradlew --include-build="$PLUGIN_PATH" $DEBUG_OPT $WITH_NOTES_OPT $PARALLEL_OPT $INFO_OPT $STACKTRACE_OPT $EXPORT_TASK
